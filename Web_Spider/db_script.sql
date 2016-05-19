1c9f27c16defeab2c6c192ce116ae544ee6aab96
Chen Mengyang <chenmengyang12@gmail.com>
2016-05-20 00:01:15 +0300

    Update db_script.sql

--- Web_Spider/db_script.sql
+++ Web_Spider/db_script.sql
@@ -1,1 +1,21 @@
-create
+create table movies
+(id int
+,title varchar(100)
+,vote_average decimal(5,2)
+,release_year smallint
+,ranking smallint
+,genres varchar(100)
+,overview varchar(1000)
+,poster_adr1 varchar(200)
+,poster_adr2 varchar(200)
+,attribute1 varchar(100)
+,attribute2 varchar(100)
+,attribute3 varchar(100)
+,attribute4 varchar(100)
+,attribute5 varchar(100)
+,attribute6 varchar(100)
+,attribute7 varchar(100)
+,attribute8 varchar(100)
+,attribute9 varchar(100)
+,attribute10 varchar(100)
+);
