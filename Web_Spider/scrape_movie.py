cb558c4a1678c79fc3051f6b424ae249015be84e
Chen Mengyang <chenmengyang12@gmail.com>
2016-05-19 22:40:31 +0300

    Create scrape_movie.py

---
+++ Web_Spider/scrape_movie.py
@@ -0,0 +1,4 @@
+# Since I am feeling so bad about using the moviedb API to get data, now we are going to scrape the data from this website: https://www.themoviedb.org/movie
+
+# create table to store the data after scraping
+# how to store the picture?
