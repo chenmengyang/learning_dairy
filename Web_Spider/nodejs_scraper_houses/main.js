1049acaec5b6eb2b3802bdc55128a39c84693ecd
Martti Chen <martti@Marttis-MacBook-Pro.local>
2017-05-15 17:14:32 +0300

    add async logic, callback problem need to be fixed

--- Web_Spider/nodejs_scraper_houses/main.js
+++ Web_Spider/nodejs_scraper_houses/main.js
@@ -1,11 +1,11 @@
 let http = require('http');
 let cheerio = require('cheerio');
 let baseUrl = 'http://www.etuovi.com/homes-for-sale?rd=50&page=';
-
+let async = require('async');
 let pageArr = [1,2,3,4,5,6,7,8,9,10];
 
-let idArr = [];
-let getId = function(currentPage)
+// let idArr = [];
+let getId = function(currentPage,callback=false)
 {
     http.get(currentPage, (res) => {
     const statusCode = res.statusCode;
@@ -29,13 +29,14 @@ let getId = function(currentPage)
         //   let parsedData = JSON.parse(rawData);
         let htmlCode = rawData;
         let $ = cheerio.load(htmlCode);
-        // let arr = [];
+        let arr = [];
         let items = $('section.results.list>ol>li') // .map(function(i,el){arr.push($(this).attr('id')); return $(this).attr('id')});//map((i,el)=>{return $(this).attr('id')});
                     .map(function(i,el){
-                        idArr.push($(this).attr('id'));
+                        arr.push($(this).attr('id'));
                         return 1;
                     });
-        console.log(idArr);
+        //console.log(currentPage + " !done! size is " + idArr.length);
+        callback(null,arr);
         //   console.log(rawData);
         } catch (e) {
         console.log(e.message);
@@ -50,9 +51,15 @@ let getFuncArr = function(func,baseUrl,pageArr)
 {
     let arr = [];
     pageArr.forEach((e)=>{
-        arr.push(func(baseUrl+e));
+        arr.push(function(){return func(baseUrl+e);});
     });
     return arr;
 }
 
-getFuncArr(getId,baseUrl,pageArr);
+// var idFuncArr = getFuncArr(getId,baseUrl,pageArr);
+// console.log(testArr[0]());
+async.parallel(getFuncArr(getId,baseUrl,pageArr),
+()=>console.log('hello')
+// function(err,results){console.log("finish! results are "+JSON.stringify(results));}
+
+);
